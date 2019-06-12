29fc768153a07f8197d61db9c942f076aeebed03
Sathish Kumar <23581693+Sathishshan@users.noreply.github.com>
2019-06-12 19:48:15 +0530

    Update shacooker.sh

--- shacooker.sh
+++ shacooker.sh
@@ -14,6 +14,6 @@ fi
 #here sed is used to avoid the ^M control character at the END of the line. ^M is equilatent to \r #carriage return.
 cat $1 | sed 's@\^M$@@g' | while read line
 do
-  find $line -exec sha1sum {} \; | awk '{print $2" "$1}' | sed 's/ /, /' >> shacooked.txt
-done
+  find $line -exec sha1sum {} \; | awk '{print $2", "$1}'
+done > shacooked.txt
 echo "Sha value has been cooked, eat fast while it is Hot."
